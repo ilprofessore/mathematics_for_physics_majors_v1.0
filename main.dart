b7f1f9b7d25c4098c3f3b34b139f840b080e6eac
Zain Ul Abideen <48854250+ilprofessore@users.noreply.github.com>
2022-04-02 00:00:44 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,272 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:mathematics_for_physics_majors/pages/calculus01.dart';
+import 'package:mathematics_for_physics_majors/pages/calculus02.dart';
+import 'package:mathematics_for_physics_majors/pages/calculus03.dart';
+import 'package:mathematics_for_physics_majors/pages/calculus04.dart';
+import 'package:mathematics_for_physics_majors/pages/calculus05.dart';
+import 'package:mathematics_for_physics_majors/pages/calculus06.dart';
+import 'package:mathematics_for_physics_majors/pages/calculus07.dart';
+import 'package:mathematics_for_physics_majors/pages/calculus08.dart';
+import 'package:mathematics_for_physics_majors/pages/calculus09.dart';
+import 'package:mathematics_for_physics_majors/pages/calculus10.dart';
+import 'package:mathematics_for_physics_majors/pages/calculus11.dart';
+import 'package:mathematics_for_physics_majors/pages/calculus12.dart';
+import 'package:mathematics_for_physics_majors/pages/essence_of_linear_algebra.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentals_of_physics_i.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentals_of_physics_ii.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi01.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi02.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi03.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi04.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi05.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi06.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi07.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi08.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi09.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi10.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi11.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi12.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi13.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi14.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi15.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi16.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi17.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi18.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi19.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi20.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi21.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi22.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi23.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsi24.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii01.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii02.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii03.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii04.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii05.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii06.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii07.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii08.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii09.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii10.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii11.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii12.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii13.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii14.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii15.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii16.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii17.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii18.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii19.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii20.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii21.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii22.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii23.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii24.dart';
+import 'package:mathematics_for_physics_majors/pages/fundamentalsofphysicsii25.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra01.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra02.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra03.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra04.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra05.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra06.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra07.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra08.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra09.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra10.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra11.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra12.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra13.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra14.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra15.dart';
+import 'package:mathematics_for_physics_majors/pages/linearalgebra16.dart';
+import 'package:mathematics_for_physics_majors/pages/mathematical_proofs.dart';
+import 'package:mathematics_for_physics_majors/pages/mathematicalproofs01.dart';
+import 'package:mathematics_for_physics_majors/pages/mathematicalproofs02.dart';
+import 'package:mathematics_for_physics_majors/pages/mathematicalproofs03.dart';
+import 'package:mathematics_for_physics_majors/pages/mathematicalproofs04.dart';
+import 'package:mathematics_for_physics_majors/pages/mathematicalproofs05.dart';
+import 'package:mathematics_for_physics_majors/pages/mathematicalproofs06.dart';
+import 'package:mathematics_for_physics_majors/pages/mathematicalproofs07.dart';
+import 'package:mathematics_for_physics_majors/pages/mathematicalproofs08.dart';
+import 'package:mathematics_for_physics_majors/pages/mathematicalproofs09.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariable_calculus.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus01.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus02.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus03.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus04.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus05.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus06.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus07.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus08.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus09.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus10.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus11.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus12.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus13.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus14.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus15.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus16.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus17.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus18.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus19.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus20.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus21.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus22.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus23.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus24.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus25.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus26.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus27.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus28.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus29.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus30.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus31.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus32.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus33.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus34.dart';
+import 'package:mathematics_for_physics_majors/pages/multivariablecalculus35.dart';
+import 'package:mathematics_for_physics_majors/pages/question_page.dart';
+import 'pages/home.dart';
+import 'pages/essence_of_calculus.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => MyApp(),
+      '/question': (context) => QuestionPage(),
+      '/calculus': (context) => EssenceOfCalculus(),
+      '/calculus01': (context) => Calculus01(),
+      '/calculus02': (context) => Calculus02(),
+      '/calculus03': (context) => Calculus03(),
+      '/calculus04': (context) => Calculus04(),
+      '/calculus05': (context) => Calculus05(),
+      '/calculus06': (context) => Calculus06(),
+      '/calculus07': (context) => Calculus07(),
+      '/calculus08': (context) => Calculus08(),
+      '/calculus09': (context) => Calculus09(),
+      '/calculus10': (context) => Calculus10(),
+      '/calculus11': (context) => Calculus11(),
+      '/calculus12': (context) => Calculus12(),
+      '/linearalgebra': (context) => EssenceOfLinearAlgebra(),
+      '/linearalgebra01': (context) => LinearAlgebra01(),
+      '/linearalgebra02': (context) => LinearAlgebra02(),
+      '/linearalgebra03': (context) => LinearAlgebra03(),
+      '/linearalgebra04': (context) => LinearAlgebra04(),
+      '/linearalgebra05': (context) => LinearAlgebra05(),
+      '/linearalgebra06': (context) => LinearAlgebra06(),
+      '/linearalgebra07': (context) => LinearAlgebra07(),
+      '/linearalgebra08': (context) => LinearAlgebra08(),
+      '/linearalgebra09': (context) => LinearAlgebra09(),
+      '/linearalgebra10': (context) => LinearAlgebra10(),
+      '/linearalgebra11': (context) => LinearAlgebra11(),
+      '/linearalgebra12': (context) => LinearAlgebra12(),
+      '/linearalgebra13': (context) => LinearAlgebra13(),
+      '/linearalgebra14': (context) => LinearAlgebra14(),
+      '/linearalgebra15': (context) => LinearAlgebra15(),
+      '/linearalgebra16': (context) => LinearAlgebra16(),
+      '/mathematicalproofs': (context) => MathematicalProofs(),
+      '/mathematicalproofs01': (context) => MathematicalProofs01(),
+      '/mathematicalproofs02': (context) => MathematicalProofs02(),
+      '/mathematicalproofs03': (context) => MathematicalProofs03(),
+      '/mathematicalproofs04': (context) => MathematicalProofs04(),
+      '/mathematicalproofs05': (context) => MathematicalProofs05(),
+      '/mathematicalproofs06': (context) => MathematicalProofs06(),
+      '/mathematicalproofs07': (context) => MathematicalProofs07(),
+      '/mathematicalproofs08': (context) => MathematicalProofs08(),
+      '/mathematicalproofs09': (context) => MathematicalProofs09(),
+      '/multivariablecalculus': (context) => MultivariableCalculus(),
+      '/multivariablecalculus01': (context) => MultivariableCalculus01(),
+      '/multivariablecalculus02': (context) => MultivariableCalculus02(),
+      '/multivariablecalculus03': (context) => MultivariableCalculus03(),
+      '/multivariablecalculus04': (context) => MultivariableCalculus04(),
+      '/multivariablecalculus05': (context) => MultivariableCalculus05(),
+      '/multivariablecalculus06': (context) => MultivariableCalculus06(),
+      '/multivariablecalculus07': (context) => MultivariableCalculus07(),
+      '/multivariablecalculus08': (context) => MultivariableCalculus08(),
+      '/multivariablecalculus09': (context) => MultivariableCalculus09(),
+      '/multivariablecalculus10': (context) => MultivariableCalculus10(),
+      '/multivariablecalculus11': (context) => MultivariableCalculus11(),
+      '/multivariablecalculus12': (context) => MultivariableCalculus12(),
+      '/multivariablecalculus13': (context) => MultivariableCalculus13(),
+      '/multivariablecalculus14': (context) => MultivariableCalculus14(),
+      '/multivariablecalculus15': (context) => MultivariableCalculus15(),
+      '/multivariablecalculus16': (context) => MultivariableCalculus16(),
+      '/multivariablecalculus17': (context) => MultivariableCalculus17(),
+      '/multivariablecalculus18': (context) => MultivariableCalculus18(),
+      '/multivariablecalculus19': (context) => MultivariableCalculus19(),
+      '/multivariablecalculus20': (context) => MultivariableCalculus20(),
+      '/multivariablecalculus21': (context) => MultivariableCalculus21(),
+      '/multivariablecalculus22': (context) => MultivariableCalculus22(),
+      '/multivariablecalculus23': (context) => MultivariableCalculus23(),
+      '/multivariablecalculus24': (context) => MultivariableCalculus24(),
+      '/multivariablecalculus25': (context) => MultivariableCalculus25(),
+      '/multivariablecalculus26': (context) => MultivariableCalculus26(),
+      '/multivariablecalculus27': (context) => MultivariableCalculus27(),
+      '/multivariablecalculus28': (context) => MultivariableCalculus28(),
+      '/multivariablecalculus29': (context) => MultivariableCalculus29(),
+      '/multivariablecalculus30': (context) => MultivariableCalculus30(),
+      '/multivariablecalculus31': (context) => MultivariableCalculus31(),
+      '/multivariablecalculus32': (context) => MultivariableCalculus32(),
+      '/multivariablecalculus33': (context) => MultivariableCalculus33(),
+      '/multivariablecalculus34': (context) => MultivariableCalculus34(),
+      '/multivariablecalculus35': (context) => MultivariableCalculus35(),
+      '/fundamentalsofphysicsi': (context) => FundamentalsOfPhysicsI(),
+      '/fundamentalsofphysicsi01': (context) => FundamentalsOfPhysicsI01(),
+      '/fundamentalsofphysicsi02': (context) => FundamentalsOfPhysicsI02(),
+      '/fundamentalsofphysicsi03': (context) => FundamentalsOfPhysicsI03(),
+      '/fundamentalsofphysicsi04': (context) => FundamentalsOfPhysicsI04(),
+      '/fundamentalsofphysicsi05': (context) => FundamentalsOfPhysicsI05(),
+      '/fundamentalsofphysicsi06': (context) => FundamentalsOfPhysicsI06(),
+      '/fundamentalsofphysicsi07': (context) => FundamentalsOfPhysicsI07(),
+      '/fundamentalsofphysicsi08': (context) => FundamentalsOfPhysicsI08(),
+      '/fundamentalsofphysicsi09': (context) => FundamentalsOfPhysicsI09(),
+      '/fundamentalsofphysicsi10': (context) => FundamentalsOfPhysicsI10(),
+      '/fundamentalsofphysicsi11': (context) => FundamentalsOfPhysicsI11(),
+      '/fundamentalsofphysicsi12': (context) => FundamentalsOfPhysicsI12(),
+      '/fundamentalsofphysicsi13': (context) => FundamentalsOfPhysicsI13(),
+      '/fundamentalsofphysicsi14': (context) => FundamentalsOfPhysicsI14(),
+      '/fundamentalsofphysicsi15': (context) => FundamentalsOfPhysicsI15(),
+      '/fundamentalsofphysicsi16': (context) => FundamentalsOfPhysicsI16(),
+      '/fundamentalsofphysicsi17': (context) => FundamentalsOfPhysicsI17(),
+      '/fundamentalsofphysicsi18': (context) => FundamentalsOfPhysicsI18(),
+      '/fundamentalsofphysicsi19': (context) => FundamentalsOfPhysicsI19(),
+      '/fundamentalsofphysicsi20': (context) => FundamentalsOfPhysicsI20(),
+      '/fundamentalsofphysicsi21': (context) => FundamentalsOfPhysicsI21(),
+      '/fundamentalsofphysicsi22': (context) => FundamentalsOfPhysicsI22(),
+      '/fundamentalsofphysicsi23': (context) => FundamentalsOfPhysicsI23(),
+      '/fundamentalsofphysicsi24': (context) => FundamentalsOfPhysicsI24(),
+      '/fundamentalsofphysicsii': (context) => FundamentalsOfPhysicsII(),
+      '/fundamentalsofphysicsii01': (context) => FundamentalsOfPhysicsII01(),
+      '/fundamentalsofphysicsii02': (context) => FundamentalsOfPhysicsII02(),
+      '/fundamentalsofphysicsii03': (context) => FundamentalsOfPhysicsII03(),
+      '/fundamentalsofphysicsii04': (context) => FundamentalsOfPhysicsII04(),
+      '/fundamentalsofphysicsii05': (context) => FundamentalsOfPhysicsII05(),
+      '/fundamentalsofphysicsii06': (context) => FundamentalsOfPhysicsII06(),
+      '/fundamentalsofphysicsii07': (context) => FundamentalsOfPhysicsII07(),
+      '/fundamentalsofphysicsii08': (context) => FundamentalsOfPhysicsII08(),
+      '/fundamentalsofphysicsii09': (context) => FundamentalsOfPhysicsII09(),
+      '/fundamentalsofphysicsii10': (context) => FundamentalsOfPhysicsII10(),
+      '/fundamentalsofphysicsii11': (context) => FundamentalsOfPhysicsII11(),
+      '/fundamentalsofphysicsii12': (context) => FundamentalsOfPhysicsII12(),
+      '/fundamentalsofphysicsii13': (context) => FundamentalsOfPhysicsII13(),
+      '/fundamentalsofphysicsii14': (context) => FundamentalsOfPhysicsII14(),
+      '/fundamentalsofphysicsii15': (context) => FundamentalsOfPhysicsII15(),
+      '/fundamentalsofphysicsii16': (context) => FundamentalsOfPhysicsII16(),
+      '/fundamentalsofphysicsii17': (context) => FundamentalsOfPhysicsII17(),
+      '/fundamentalsofphysicsii18': (context) => FundamentalsOfPhysicsII18(),
+      '/fundamentalsofphysicsii19': (context) => FundamentalsOfPhysicsII19(),
+      '/fundamentalsofphysicsii20': (context) => FundamentalsOfPhysicsII20(),
+      '/fundamentalsofphysicsii21': (context) => FundamentalsOfPhysicsII21(),
+      '/fundamentalsofphysicsii22': (context) => FundamentalsOfPhysicsII22(),
+      '/fundamentalsofphysicsii23': (context) => FundamentalsOfPhysicsII23(),
+      '/fundamentalsofphysicsii24': (context) => FundamentalsOfPhysicsII24(),
+      '/fundamentalsofphysicsii25': (context) => FundamentalsOfPhysicsII25(),
+
+
+
+    },
+  ));
+}
